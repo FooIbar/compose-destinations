8f1024ee1b580a110673b09c048e612bc930b1b2
FooIbar <118464521+FooIbar@users.noreply.github.com>
2024-10-09 08:12:47 +0000

    Try KSP2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
     }
 }
 
